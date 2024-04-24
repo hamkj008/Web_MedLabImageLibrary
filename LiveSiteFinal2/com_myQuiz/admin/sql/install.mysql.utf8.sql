ac8c9f7a53b3a92f1be3774b4f38c099959e19c6
hamkj008 <kieran.h01@protonmail.com>
2024-04-24 18:13:33 +0930

    button hover change

--- LiveSiteFinal2/com_myQuiz/admin/sql/install.mysql.utf8.sql
+++ LiveSiteFinal2/com_myQuiz/admin/sql/install.mysql.utf8.sql
@@ -61,3 +61,29 @@ CREATE TABLE IF NOT EXISTS `#__myQuiz_quizUserSummary` (
   FOREIGN KEY (`userId`) REFERENCES `#__users` (`id`) ON DELETE CASCADE,
   FOREIGN KEY (`quizId`) REFERENCES `#__myQuiz_quiz` (`id`)  ON DELETE CASCADE
 ) ENGINE = InnoDB;
+
+
+
+INSERT INTO `#__myQuiz_quiz` (`imageId`, `title`, `description`, `attemptsAllowed`) VALUES
+	(1, 'Example Quiz', 'This is an example to quiz to demonstrate the functionality', 5);
+
+INSERT INTO `#__myQuiz_question` (`quizId`, `description`, `feedback`) VALUES
+	(1, 'Is this question example number 1?', 'This is the description area for question example number 1.'),
+	(1, 'Is this question example number 2?', 'This is the description area for question example number 2.'),
+	(1, 'Is this question example number 3?', 'This is the description area for question example number 3.'),
+  (1, 'Is this question example number 4?', 'This is the description area for question example number 4.');
+	
+INSERT INTO `#__myQuiz_answer` (`questionId`, `description`, `markValue`) VALUES
+	(1, 'Yes', 1),
+	(1, 'No', 0),
+  (1, 'I do not know', 0),
+	(2, 'The answer is A', 1),
+	(2, 'The answer is B', 0),
+	(2, 'The answer is C', 0),
+	(3, 'Yes, completely', 0),
+	(3, 'Yes, but only partially', 1),
+	(3, 'No, not at all', 0),
+  (4, '10', 0),
+  (4, '50', 1),
+  (4, '100', 2),
+  (4, '1000', 3);
